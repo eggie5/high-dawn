4b609d7bce706e4c48a5fd49c3b0de8de2fe7c5f
Alex Egg <eggie5@gmail.com>
2012-05-16 23:36:54 -0700

    added readme and sample script

--- high-dawn.rb
+++ high-dawn.rb
@@ -3,4 +3,16 @@ require 'ap'
 
 
 
-ap Twitter.user("rolandal")
+friends=Twitter.friend_ids("rolandal")
+followers=Twitter.follower_ids("rolandal")
+
+
+
+frc=Twitter.user("rolandal").friends_count
+ap "@rolandal is following #{frc} people,"
+
+foc=Twitter.user("rolandal").followers_count
+ap "but only has #{foc} followers"
+
+delta=frc-foc
+ap "#{delta} people are not following him back..."
