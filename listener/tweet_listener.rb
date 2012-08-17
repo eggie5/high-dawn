777d6d347c4167f9f0056cc2cfbcd168c901110e
Alex Egg <eggie5@gmail.com>
2012-08-16 20:09:27 -0700

    restored listeener

--- listener/tweet_listener.rb
+++ listener/tweet_listener.rb
@@ -8,25 +8,30 @@ require 'resque'
 require "net/http"
 require "uri"
 
+# Start the log over whenever the log exceeds 100 megabytes in size.
+$LOG = Logger.new('tweet_listener.log', 0, 100 * 1024 * 1024)
+$LOG.formatter = proc do |severity, datetime, progname, msg|
+  "#{datetime}: #{msg}\n"
+end
+
 module HighDawn
   class Listener
     def initialize()
-      # Start the log over whenever the log exceeds 100 megabytes in size.
-      $LOG = Logger.new('tweet_listener.log', 0, 100 * 1024 * 1024)
     end
 
     def self.log(msg)
       p msg
-      $LOG.debug("#{msg} \n")
+      $LOG.info(msg)
     end
 
     def self.ids
       if(ENV["HD_ENV"]=="production")
+        self.log "Running in production!"
         url="http://high-dawn-7765.herokuapp.com/uids.json"
       else
         url="http://localhost:5200/uids.json"
       end
-      
+
       #collect list of all non_bros for every user
       uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)
@@ -37,21 +42,21 @@ module HighDawn
         user_ids=user.non_bros.ids
         uids.concat user_ids
       end
-
+      self.log "found #{uids.length} uids to monitor"
       uids
     end
 
 
     def listen_to(non_bros)
-      p "listening..."
+      Listener.log "listening..."
       client=TweetStream::Client.new
 
       client.on_limit do |skip_count|
-        p "rate limit. skip_count=#{skip_count}"
+        Listener.log "rate limit. skip_count=#{skip_count}"
       end
 
       client.on_error do |message|
-        p "error: #{message}"
+        Listener.log "error: #{message}"
       end
 
       client.follow(non_bros) do |tweet|
@@ -63,11 +68,12 @@ module HighDawn
         HighDawn::TweetModel.cache_id non_bro.id, tweet.user.screen_name
 
         next if htweet.retweet? #dont care about retweets
-        p obj={id: tweet.id, uid: tweet.user.id, text: tweet.text }
-        # puts "."
+        obj={uid: tweet.user.id, text: tweet.text }
+        Listener.log obj 
 
         #get a list of users following this tweet's owner
-        p followers = non_bro.followers
+        followers = non_bro.followers
+        Listener.log followers unless followers.empty?
         followers.each do |id|
           u=HighDawn::User.new id
           u.queue << htweet
